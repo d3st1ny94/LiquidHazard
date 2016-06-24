5793c31f85d4e292f8db5f3ab4e8eee6b6cb4eec
d3st1ny94 <adan.delapena@gmail.com>
2016-06-24 14:50:39 -0400

    Add files via upload

---
+++ LiquidHazard/LiquidFun.h
@@ -0,0 +1,39 @@
+//
+//  LiquidFun.h
+//  LiquidHazard
+//
+//  Created by adan de la pena on 6/20/16.
+//  Copyright © 2016 adan de la pena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef LiquidFun_Definitions
+#define LiquidFun_Definitions
+
+typedef struct Vector2D {
+  float x;
+  float y;
+} Vector2D;
+
+typedef struct Size2D {
+  float width;
+  float height;
+} Size2D;
+
+#endif
+
+@interface LiquidFun : NSObject
+
++ (void)createWorldWithGravity:(Vector2D)gravity;
++ (void *)createParticleSystemWithRadius:(float)radius dampingStrength:(float)dampingStrength gravityScale:(float)gravityScale density:(float)density;
++ (void)createParticleBoxForSystem:(void *)particleSystem position:(Vector2D)position size:(Size2D)size;
++ (int)particleCountForSystem:(void *)particleSystem;
++ (void *)particlePositionsForSystem:(void *)particleSystem;
++ (void)worldStep:(CFTimeInterval)timeStep velocityIterations:(int)velocityIterations
+positionIterations:(int)positionIterations;
++ (void *)createEdgeBoxWithOrigin:(Vector2D)origin size:(Size2D)size;
++ (void)setGravity:(Vector2D)gravity;
++ (void)setParticleLimitForSystem:(void *)particleSystem maxParticles:(int)maxParticles;
+
+@end
