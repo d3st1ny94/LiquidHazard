73ae80edc61b563ed36047bea04729e86f592326
d3st1ny94 <adan.delapena@gmail.com>
2016-06-24 14:49:22 -0400

    Add files via upload

---
+++ LiquidHazard/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LiquidFun.h
+//  LiquidHazard
+//
+//  Created by adan de la pena on 6/20/16.
+//  Copyright © 2016 adan de la pena. All rights reserved.
+//
+
+#ifndef LiquidMetal_Bridging_Header_h
+#define LiquidMetal_Bridging_Header_h
+
+#import "LiquidFun.h"
+
+#endif
